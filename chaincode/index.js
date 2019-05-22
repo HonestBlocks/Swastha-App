bde4537d90927d0f8e8d387dc33e9f976523b403
Gaurav <gaurav.ma1503@ict.gnu.ac.in>
2019-05-22 09:40:23 +0530

    Api&Contract

---
+++ chaincode/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const SwasthaContract = require('./lib/fabcar');
+
+module.exports.SwasthaContract = SwasthaContract;
+module.exports.contracts = [ SwasthaContract ];
